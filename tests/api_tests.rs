9f87dab08789761afb8d41932ea8c10b55f3ce05
Musk Zhang <muskzhang2024@gmail.com>
2024-11-15 19:06:31 +0800

    提取了一个init service的方法

--- tests/api_tests.rs
+++ tests/api_tests.rs
@@ -1,14 +1,12 @@
-use ntex::http::Request;
-use ntex::{Pipeline, Service};
+use crate::common::init_test_service;
 use ntex::util::Bytes;
-use ntex::web::{self, test, test::TestRequest, Error, WebResponse};
+use ntex::web::{test, test::TestRequest};
 
 mod common;
 
 #[ntex::test]
 async fn test_hello_world() {
-    let app =
-        init_test_service().await;
+    let app = init_test_service().await;
 
     let req = TestRequest::get().uri("/").to_request();
     let resp = test::call_service(&app, req).await;
@@ -25,14 +23,9 @@ async fn test_hello_world() {
     assert_eq!(body, Bytes::from_static(b"Hello world!"));
 }
 
-async fn init_test_service() -> Pipeline<impl Service<Request, Response=WebResponse, Error=Error> + Sized> {
-    test::init_service(web::App::new().route("/", web::get().to(ntex_api::api::index))).await
-}
-
 #[ntex::test]
 async fn test_not_found() {
-    let app =
-        test::init_service(web::App::new().route("/", web::get().to(ntex_api::api::index))).await;
+    let app = init_test_service().await;
 
     let req = TestRequest::get().uri("/not-exists").to_request();
     let resp = test::call_service(&app, req).await;
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,6 +2,6 @@ use ntex::http::Request;
 use ntex::{web, Pipeline, Service};
 use ntex::web::{test, Error, WebResponse};
 
-async fn init_test_service() -> Pipeline<impl Service<Request, Response=WebResponse, Error=Error> + Sized> {
+pub async fn init_test_service() -> Pipeline<impl Service<Request, Response=WebResponse, Error=Error> + Sized> {
     test::init_service(web::App::new().route("/", web::get().to(ntex_api::api::index))).await
 }
